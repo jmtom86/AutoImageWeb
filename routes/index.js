944d57460089625a383ed3271e7b031edfe09586
Jordan <jmtom86@yahoo.com>
2018-04-06 00:33:26 -0700

    added emailing module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+	host: 'smtp.mail.yahoo.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: 'jmtom86@yahoo.com',
+		pass: 'Peanut4495@'
+	}
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,5 +24,30 @@ router.get('/services', function(req, res, next) {
 router.get('/services1', function(req, res, next) {
 	res.render('services1');
 })
+
+router.get('/quote', function(req, res, next) {
+	res.render('quote');
+})
+
+router.post('/sendquote', function(req, res, next) {
+	console.log(req.body);
+	res.render('quote');
+	let mailOptions = {
+		from: "" + req.body.email,
+		to: "jmtom86@yahoo.com",
+		subject: "Hello",
+		text: "" + req.body.comment,
+		replyTo: req.body.email
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if(error) {
+			return console.log(error);
+		}
+		console.log('Message sent: %s', info.messageId);
+	})
+})
+
+
 
 module.exports = router;
