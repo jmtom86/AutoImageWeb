04529b19787777b6eb0303bfd71123c62770af48
Jordan <jmtom86@yahoo.com>
2018-07-08 19:53:35 -0700

    worked on quote page and cleaning up unused code

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-	host: 'smtp.mail.yahoo.com',
+	host: 'smtp.gmail.com',
 	port: 465,
 	secure: true,
 	auth: {
-		user: 'jmtom86@yahoo.com',
-		pass: 'Peanut4495@'
-	}
+		user: 'tickleburnt@gmail.com',
+		pass: 'peanut420'
+	},
+	tls: { rejectUnauthorized: false }
 })
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,20 +32,20 @@ router.get('/quote', function(req, res, next) {
 
 router.post('/sendquote', function(req, res, next) {
 	console.log(req.body);
-	res.render('quote');
 	let mailOptions = {
-		from: "" + req.body.email,
-		to: "jmtom86@yahoo.com",
-		subject: "Hello",
-		text: "" + req.body.comment,
+		from: "contact_form@autoimagetint.com",
+		to: "tickleburnt@gmail.com",
+		subject: "New Quote Inquiry: Window Tinting",
+		html: "<h3>You have received a quote inquiry from "+req.body.fname + " " + req.body.lname + ". Here is the message</h3><p>First Name: "+req.body.fname + "<br>Last Name: "+req.body.lname+"<br>Email: "+req.body.email+"<br>Phone: "+req.body.phone+"<br>Make: "+req.body.make+"<br>Model: "+req.body.model+"<br>Year: "+req.body.year+"<br>Further Questions and Comments: "+req.body.comment+"</p>",
 		replyTo: req.body.email
 	};
-
+	console.log("SENDING MAIL")
 	transporter.sendMail(mailOptions, (error, info) => {
 		if(error) {
-			return console.log(error);
+			console.log("SENT FAIL");
+			 console.log(error);
 		}
-		console.log('Message sent: %s', info.messageId);
+		// console.log('Message sent: %s', info.messageId);
 	})
 })
 
